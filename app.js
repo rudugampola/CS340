345eecb13515632ecd377da9af90140e28ca8d80
Ravindu Udugampola <udugampr@flip2.engr.oregonstate.edu>
2022-10-03 22:07:04 -0700

    🚀

--- app.js
+++ app.js
@@ -6,42 +6,31 @@ var express = require('express');   // We are using the express library for the 
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 8000;                 // Set a port number at the top so it's easy to change in the future
 
-// Database
-var db = require('./db-connector')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// SQL Database
+var db = require('./db-connector');
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
 /*
     ROUTES
 */
+// app.js 
+
 app.get('/', function(req, res)
     {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
+        let query1 = "SELECT * FROM Customers;";               // Define our query
 
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        let base = "<h1>MySQL Results:</h1>"
-                        res.send(base + JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });
+            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                   // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
 /*
     LISTENER
